f5715df995836302a49dd2798f92f719f3f4c9e3
zengqiang.fang <zengqiang.fang@thoughtworks.com>
2022-03-31 10:43:56 +0800

    实现需求一

--- src/test/java/com/thoughtworks/codepairing/model/ShoppingCartTest.java
+++ src/test/java/com/thoughtworks/codepairing/model/ShoppingCartTest.java
@@ -73,4 +73,22 @@ public class ShoppingCartTest {
 
         assertEquals(6, order.getLoyaltyPoints());
     }
+
+    @Test
+    public void shouldCalculatePriceFor20PercentDiscount() {
+        List<Product> products = asList(new Product(PRICE, "DIS_20_ABCD", PRODUCT));
+        ShoppingCart cart = new ShoppingCart(customer, products);
+        Order order = cart.checkout();
+
+        assertEquals(80.0, order.getTotalPrice(), 0.0);
+    }
+
+    @Test
+    public void shouldCalculateLoyaltyPointsFor20PercentDiscount() {
+        List<Product> products = asList(new Product(PRICE, "DIS_20_ABCD", PRODUCT));
+        ShoppingCart cart = new ShoppingCart(customer, products);
+        Order order = cart.checkout();
+
+        assertEquals(5, order.getLoyaltyPoints());
+    }
 }
